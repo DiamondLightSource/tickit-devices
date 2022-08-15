5dc30ecc67da6c115a8fc48b374d460bac79085a
Abigail Emery <abigail.emery@diamond.ac.uk>
2022-08-15 13:23:52 +0100

    Add build CI (#7)
    * Add build CI
    
    * Add workflow triggers
    
    * Change workflow for url versioned dependency
    
    * Limit branch CI to avoid duplicates
    
    * Fix indentation
    
    * Simplify setup.py
    
    * Update README.rst to include code ci

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import glob
-import importlib.util
-
 from setuptools import setup
 
-# Import <package>._version_git.py without importing <package>
-path = glob.glob(__file__.replace("setup.py", "src/*/_version_git.py"))[0]
-spec = importlib.util.spec_from_file_location("_version_git", path)
-vg = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(vg)
-
-setup(cmdclass=vg.get_cmdclass(), version=vg.__version__)
+if __name__ == "__main__":
+    setup()
