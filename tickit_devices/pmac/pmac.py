165142b6222cba37af461fa81d8bef6587a9c850
Matthew Pritchard <matthew.pritchard@diamond.ac.uk>
2022-09-08 16:32:19 +0100

    #18: Add minimal PMAC device for dls-pmac-control startup

--- tickit_devices/pmac/pmac.py
+++ tickit_devices/pmac/pmac.py
@@ -2,12 +2,11 @@ from dataclasses import dataclass
 from typing import Dict
 
 from tickit.adapters.composed import ComposedAdapter
-from tickit.adapters.interpreters.command import CommandInterpreter
+from tickit.adapters.interpreters.command import MultiCommandInterpreter
 from tickit.adapters.interpreters.command.regex_command import RegexCommand
 from tickit.adapters.interpreters.wrappers import (
     BeheadingInterpreter,
     JoiningInterpreter,
-    SplittingInterpreter,
 )
 from tickit.adapters.servers.tcp import TcpServer
 from tickit.core.components.component import Component, ComponentConfig
@@ -17,6 +16,7 @@ from tickit.core.typedefs import SimTime
 from tickit.utils.byte_format import ByteFormat
 from tickit.utils.compat.typing_compat import TypedDict
 
+# Some constants copied from the old PMAC sim.
 M_TRAJ_VERSION = 4049
 M_TRAJ_BUFSIZE = 4037
 M_TRAJ_A_ADR = 4041
@@ -49,22 +49,15 @@ class PMACDevice(Device):
         self.mvars[72] = 2621
         self.mvars[73] = 76
         self.pvars: Dict = dict()
-        self.axes = {
-            1: PMACAxis(),
-            2: PMACAxis(),
-            3: PMACAxis(),
-            4: PMACAxis(),
-            5: PMACAxis(),
-            6: PMACAxis(),
-            7: PMACAxis(),
-            8: PMACAxis(),
-        }
+        self.axes = {i: PMACAxis() for i in range(1, 17)}
         self.system_ivars: Dict = dict()
         self.system_ivars[20] = "$78400"
         self.system_ivars[21] = "$0"
         self.system_ivars[22] = "$0"
         self.system_ivars[23] = "$0"
         self.other_ivars: Dict = dict()
+        self.current_axis = 1
+        self.current_cs = 1
 
     def update(self, time: SimTime, inputs: Inputs) -> DeviceUpdate[Outputs]:
         print("Updating\n")
@@ -91,41 +84,39 @@ class PMACAdapter(ComposedAdapter):
             port (Optional[int]): The bound port of the TcpServer. Defaults to 1025.
         """
         super().__init__(
-            TcpServer(host, port, ByteFormat(b"%b\n")),
+            TcpServer(host, port, ByteFormat(b"%b\x06")),
             BeheadingInterpreter(
-                JoiningInterpreter(
-                    SplittingInterpreter(CommandInterpreter(), b" "), b""
-                ),
+                JoiningInterpreter(MultiCommandInterpreter(), b""),
                 header_size=8,
             ),
         )
 
-    @RegexCommand(rb"\r?\n?")
-    async def parse_just_bytes(self):
+    @RegexCommand(rb"\r?\n?$")
+    async def end_of_message(self):
         return b"\r"
 
-    @RegexCommand(rb"(?i:CID)\r?\n?")
+    @RegexCommand(rb"(?i:CID)")
     async def get_cid(self):
+        """Regex bytestring command returning the value for the PMACA's card ID number.
+
+        Note:
+        602404: "Turbo PMAC2 Clipper",
+        602413: "Turbo PMAC2-VME",
+        603382: "Geo Brick (3U Turbo PMAC2)",
+        """
         return b"603382\r"
 
-    @RegexCommand(rb"(?i:CPU)\r?\n?")
+    @RegexCommand(rb"(?i:CPU)")
     async def get_cpu(self):
+        """Command reporting the PMAC's CPU type."""
         return b"DSP56321\r"
 
-    @RegexCommand(rb"#([1-8])[pP]\r?\n?")
-    async def get_axis_position(self, axis_no: int):
-        return str(1.0).encode() + b"1\r"
+    @RegexCommand(rb"(?i:VER)")
+    async def get_ver(self):
+        """Command reporting the PMAC's firmware version."""
+        return b"1.947  \r"
 
-    @RegexCommand(rb"#([1-8])[fF]\r?\n?")
-    async def get_axis_following_error(self):
-        # target - current
-        return b"0\r"
-
-    @RegexCommand(rb"#([1-8])\?\r?\n?")
-    async def get_axis_status(self):
-        return b"880000018401\r"
-
-    @RegexCommand(rb"[mM]([0-9]{1,5})\r?\n?")  # 1-4 or 1-5?
+    @RegexCommand(rb"[mM]([0-9]{1,5})")
     async def get_m_var(self, mvar: int):
         """Regex bytestring command that returns the value of a specific mvar.
 
@@ -137,7 +128,7 @@ class PMACAdapter(ComposedAdapter):
             self.device.mvars[mvar] = 0
         return f"{self.device.mvars[mvar]}\r".encode()
 
-    @RegexCommand(rb"[mM]([0-9]{1,5})=([0-9]*.?[0-9]*)\r?\n?")
+    @RegexCommand(rb"[mM]([0-9]{1,5})=([0-9]*.?[0-9]*)")
     async def set_m_var(self, mvar: int, value: float):
         """Regex bytestring command that sets the value of a specific mvar.
 
@@ -146,9 +137,9 @@ class PMACAdapter(ComposedAdapter):
             value (float): the value for the mvar to be set to.
         """
         self.device.mvars[mvar] = value
-        return b"\r"
+        return b""
 
-    @RegexCommand(rb"[pP]([0-9]{1,5})\r?\n?")  # 1-4 or 1-5?
+    @RegexCommand(rb"[pP]([0-9]{1,5})")
     async def get_p_var(self, pvar: int):
         """Regex bytestring command that returns the value of a specific pvar.
 
@@ -160,7 +151,7 @@ class PMACAdapter(ComposedAdapter):
             self.device.pvars[pvar] = 0
         return f"{self.device.pvars[pvar]}\r".encode()
 
-    @RegexCommand(rb"[pP]([0-9]{1,5})=([0-9]*.?[0-9]*)\r?\n?")
+    @RegexCommand(rb"[pP]([0-9]{1,5})=([0-9]*.?[0-9]*)")
     async def set_p_var(self, pvar: int, value: float):
         """Regex bytestring command that sets the value of a specific pvar.
 
@@ -169,9 +160,9 @@ class PMACAdapter(ComposedAdapter):
             value (float): the value for the pvar to be set to.
         """
         self.device.pvars[pvar] = value
-        return b"\r"
+        return b""
 
-    @RegexCommand(rb"[iI]([0-9]{1,2})\r?\n?")
+    @RegexCommand(rb"[iI]([0-9]{1,2})")
     async def read_system_ivar(self, ivar: int):
         """Regex bytestring command that returns the value of a specific system ivar.
 
@@ -183,7 +174,7 @@ class PMACAdapter(ComposedAdapter):
             self.device.system_ivars[ivar] = 0
         return f"{self.device.system_ivars[ivar]}\r".encode()
 
-    @RegexCommand(rb"[iI]([0-9]{1,2})=\$?(\d+(?:.\d+)?)\r?\n?")
+    @RegexCommand(rb"[iI]([0-9]{1,2})=\$?(\d+(?:.\d+)?)")
     async def write_system_ivar(self, ivar: int, value: float):
         """Regex bytestring command that sets the value of a specific system ivar.
 
@@ -192,9 +183,9 @@ class PMACAdapter(ComposedAdapter):
             value (float): the value to set the ivar to.
         """
         self.device.system_ivars[ivar] = value
-        return b"\r"
+        return b""
 
-    @RegexCommand(rb"[iI]([0-9])([0-9]{2})\r?\n?")
+    @RegexCommand(rb"[iI](1*[0-9])([0-9]{2})")
     async def read_axis_var(self, axis: int, ivar: int):
         """Regex bytestring command that returns the value of a specific axis ivar.
 
@@ -207,7 +198,7 @@ class PMACAdapter(ComposedAdapter):
             self.device.axes[axis].ivars[ivar] = 0
         return f"{self.device.axes[axis].ivars[ivar]}\r".encode()
 
-    @RegexCommand(rb"[iI]([0-9])([0-9]{2})=-?(\d+(?:.\d+)?)\r?\n?")
+    @RegexCommand(rb"[iI](1*[0-9])([0-9]{2})=-?(\d+(?:.\d+)?)")
     async def write_axis_ivar(self, axis: int, ivar: int, value: float):
         """Regex bytestring command that sets the value of a specific axis ivar.
 
@@ -217,9 +208,9 @@ class PMACAdapter(ComposedAdapter):
             value (float): the value to set the ivar to.
         """
         self.device.axes[axis].ivars[ivar] = value
-        return b"\r"
+        return b""
 
-    @RegexCommand(rb"[iI]([0-9]{4})\r?\n?")
+    @RegexCommand(rb"[iI]([0-9]{4})")
     async def read_other_ivar(self, ivar: int):
         """Regex bytestring command that returns the value of a specific ivar.
 
@@ -231,7 +222,7 @@ class PMACAdapter(ComposedAdapter):
             self.device.other_ivars[ivar] = 0
         return f"{self.device.other_ivars[ivar]}\r".encode()
 
-    @RegexCommand(rb"[iI]([0-9]{4})=-?(\d+(?:\.\d+)?)\r?\n?")
+    @RegexCommand(rb"[iI]([0-9]{4})=-?(\d+(?:\.\d+)?)")
     async def write_other_var(self, ivar: int, value: float):
         """Regex bytestring command that sets the value of a specific ivar.
 
@@ -240,11 +231,93 @@ class PMACAdapter(ComposedAdapter):
             value (float): the value to set the ivar to.
         """
         self.device.other_ivars[ivar] = value
-        return b"\r"
+        return b""
 
-    @RegexCommand(rb"\?\?\?\r?\n?")
-    async def get_status(self):
+    @RegexCommand(rb"#")
+    async def get_current_axis(self):
+        """Command reporting the currently addressed axis."""
+        return f"{self.device.current_axis}\r".encode()
+
+    @RegexCommand(rb"#(1*[0-9])")
+    async def set_current_axis(self, axis: int):
+        """Command setting the currently addressed axis.
+
+        Args:
+            axis (int): the new current axis.
+        """
+        self.device.current_axis = axis
+        return b""
+
+    @RegexCommand(rb"\?")
+    async def get_axis_status(self):
+        """Command getting the status of the currently addressed axis.
+
+        Currently returns a dummy value of the right format.
+        """
         return b"000000000000\r"
+
+    @RegexCommand(rb"&")
+    async def get_current_cs(self):
+        """Command reporting the currently addressed coordinate system."""
+        return f"{self.device.current_cs}".encode()
+
+    @RegexCommand(rb"&([1-8])")
+    async def set_current_cs(self, cs: int):
+        """Command setting the currently addressed coordinate system.
+
+        Args:
+            axis (int): the new active coordinate system.
+        """
+        self.device.current_cs = cs
+        return b""
+
+    @RegexCommand(rb"\?\?")
+    async def get_cs_status(self):
+        """Command reporting the status of the currently addressed coordinate system.
+
+        Currently returns a dummy value of the right format.
+        """
+        return b"A80020000000000000\r"
+
+    @RegexCommand(rb"\?\?\?")
+    async def get_status(self):
+        """Command reporting the status of the PMAC.
+
+        Currently returns a dummy value of the right format.
+        """
+        return b"000000000000\r"
+
+    @RegexCommand(rb"%")
+    async def get_cs_feedrate_override(self):
+        """Command reporting the feedrate override of the current coordinate system.
+
+        Currently returns a dummy value of the right format.
+        """
+        return b"100\r"
+
+    @RegexCommand(rb"P")
+    async def get_axis_position(self):
+        """Command reporting the motor position of the currently addressed axis.
+
+        Currently returns a dummy value of the right format.
+        """
+        return b"1.0\r"
+
+    @RegexCommand(rb"V")
+    async def get_axis_velocity(self):
+        """Command reporting the motor velocity of the currently addressed axis.
+
+        Currently returns a dummy value of the right format.
+        """
+        return b"0.0\r"
+
+    @RegexCommand(rb"F")
+    async def get_axis_follow_error(self):
+        """Command reporting the motor following error of the current axis.
+
+        Currently returns a dummy value of the right format.
+        """
+        return b"0.0\r"
 
 
 @dataclass
