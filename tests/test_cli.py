6d9345d2cef17d59b1e19705a0fac2eeff5d6e3e
Abi Emery <>
2023-02-17 14:51:48 +0000

    Fix cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,5 +5,5 @@ from tickit_devices import __version__
 
 
 def test_cli_version():
-    cmd = [sys.executable, "-m", "tickit-devices", "--version"]
+    cmd = ["tickit-devices", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
