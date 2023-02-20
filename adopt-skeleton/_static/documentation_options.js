b0d2433de6b3047df3e130e721ac6913fb9104b6
abbiemery <abbiemery@users.noreply.github.com>
2023-02-20 18:40:55 +0000

    deploy: 164059ae70d7fac98f00362ca4487c607795f5f9

--- adopt-skeleton/_static/documentation_options.js
+++ adopt-skeleton/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0rc1.dev81+gf2c44b7',
+    VERSION: '0.1.0rc1.dev82+g164059a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
