1a7d7a74ba4ca683fa9e304ca216e46ee257ba00
Abigail Emery <abigail.emery@diamond.ac.uk>
2023-02-20 21:33:53 +0000

    Update synchrotron db file paths (#38)

--- src/tickit_devices/synchrotron/synchrotron_current.py
+++ src/tickit_devices/synchrotron/synchrotron_current.py
@@ -166,7 +166,7 @@ class SynchrotronCurrent(ComponentConfig):
     host: str = "localhost"
     port: int = 25565
     format: ByteFormat = ByteFormat(b"%b\r\n")
-    db_file: str = "tickit_devices/synchrotron/db_files/DCCT.db"
+    db_file: str = "src/tickit_devices/synchrotron/db_files/DCCT.db"
     ioc_name: str = "BL03S-SR-DI-DCCT-01"
 
     def __call__(self) -> Component:  # noqa: D102
