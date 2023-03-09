7cf375cf61078ca56500e3c059e7323125e39285
dperl-dls <dperl-dls@users.noreply.github.com>
2023-03-09 12:53:23 +0000

    deploy: b5f35ec350e4d9e6883e117c8dfecdaf1320c416

--- artemis516_458_remove_prefix_from_synchrotron_devices/_static/documentation_options.js
+++ artemis516_458_remove_prefix_from_synchrotron_devices/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev8+gaa0f377',
+    VERSION: '0.1.1.dev9+gb5f35ec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
