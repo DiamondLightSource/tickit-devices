b29f20d459f8c9475d192125bd2edb8efdc89e87
abbiemery <abbiemery@users.noreply.github.com>
2023-06-16 14:26:00 +0000

    deploy: 78b72beefcf3cc7d4bc0d541da6bc2ab65f39500

--- eiger-exts-logic/_static/documentation_options.js
+++ eiger-exts-logic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev24+gf6eecf3',
+    VERSION: '0.1.1.dev25+g78b72be',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
