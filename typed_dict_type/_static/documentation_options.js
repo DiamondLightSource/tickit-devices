e016a3e7e3758e26aeccb1af65aac94753efceea
tpoliaw <tpoliaw@users.noreply.github.com>
2023-07-06 14:08:11 +0000

    deploy: 1007b46000d20b546e004c4c1c8d8a678e61c099

--- typed_dict_type/_static/documentation_options.js
+++ typed_dict_type/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev25+g5f5c8da',
+    VERSION: '0.1.1.dev26+g1007b46',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
