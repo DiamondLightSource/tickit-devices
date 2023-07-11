adfb3345273a2d903fbef4d73aab1e13fc4b00a9
Joseph Ware <joseph.ware@diamond.ac.uk>
2023-07-11 09:39:41 +0100

    Move zebra-device branch from tickit to tickit-devices

---
+++ src/tickit_devices/zebra/zebra.py
@@ -0,0 +1,65 @@
+from typing import Dict
+
+from tickit.adapters.composed import ComposedAdapter
+from tickit.adapters.interpreters.command import RegexCommand
+from tickit.adapters.servers.tcp import TcpServer
+from tickit.core.components.system_simulation import SystemSimulationComponent
+from tickit.core.typedefs import ComponentID
+
+from . import AndOrBlockConfig
+from ._common import param_types, register_names, mux_types, Block
+
+
+class ZebraDevice(SystemSimulationComponent):
+    blocks = Dict[str, Block]
+    params: Dict[str, int]
+    muxes: Dict[str, int]
+    server: TcpServer
+
+    def __init__(self, name: ComponentID, host: str = "localhost", port: int = 7012):
+        self.params = {param: 0 for param in param_types}
+        self.muxes = {mux: 0 for mux in mux_types}
+        self.blocks = {
+            block.name: block for block in [
+                AndOrBlockConfig(ComponentID(f"{ANDOR}{NUM + 1}"), self.params)
+                for ANDOR in {"AND", "OR"} for NUM in range(4)
+            ]
+        }
+        self.server = TcpServer(host, port)
+        super().__init__(
+            name=name,
+            expose={},
+            components=[block for block in self.blocks.values()],
+        )
+
+    def set_reg(self, reg: int, value: int):
+        reg_name = register_names[reg]
+        if reg_name in self.params:
+            self.params[reg_name] = value
+            for block_name in param_types[reg_name].blocks:
+                self.components[block_name].raise_interrupt()
+        else:
+            self.muxes[reg_name] = value
+
+    def get_reg(self, reg: int) -> int:
+        reg_name = register_names[reg]
+        try:
+            return self.params[reg_name]
+        except KeyError:
+            return self.muxes[reg_name]
+
+
+class ZebraAdapter(ComposedAdapter[bytes, ZebraDevice]):
+    """network adapter for zebra system simulation"""
+
+    @RegexCommand(br"W([0-9A-F]{2})([0-9A-F]{4})\n", interrupt=True)
+    async def set_reg(self, reg: str, value: str) -> bytes:
+        reg_int, value_int = int(reg, base=16), int(value, base=16)
+        self.device.set_reg(reg_int, value_int)
+        return b"W%02XOK" % reg_int
+
+    @RegexCommand(br"R([0-9A-F]{2})\n")
+    async def get_reg(self, reg: str) -> bytes:
+        reg_int = int(reg, base=16)
+        value_int = self.device.get_reg(reg_int)
+        return b"R%02X%04XOK" % (reg_int, value_int)
---
+++ tests/zebra/test_zebra.py
@@ -0,0 +1,37 @@
+import pytest
+from tickit.core.typedefs import ComponentID, SimTime
+
+from tickit_devices.zebra import ZebraDevice
+from tickit_devices.zebra.and_or_block import AndOrBlockConfig
+
+
+@pytest.fixture
+def zebra() -> ZebraDevice:
+    return ZebraDevice(ComponentID("zebra"))
+
+
+all_true = 15
+one_false = 14
+one_true = 1
+all_false = 0
+varying_values_of_true = {all_false, one_true, one_false, all_true}
+
+
+# # # # # AndOrBlock Tests # # # # #
+
+@pytest.mark.parametrize("enabled", varying_values_of_true)
+@pytest.mark.parametrize("inverted", varying_values_of_true)
+def test_all_true(enabled: int, inverted: int):
+    name = ComponentID("AND1")
+    params = {
+        f"{name}_ENA": enabled,
+        f"{name}_INV": inverted
+    }
+    mux = {
+        f"INP{i+1}": True for i in range(4)
+    }
+    block = AndOrBlockConfig(name, params)()
+    out = block.on_tick(SimTime(0), mux)
+    assert out.outputs == {"OUT": enabled == all_true and inverted == all_false or enabled + inverted == all_true}
+
+
