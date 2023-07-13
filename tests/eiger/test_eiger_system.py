e186dc4c5b3752156582eff84d5fd7c1f7d8ed64
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-07-13 08:37:07 +0100

    Tidy up eiger system test

--- tests/eiger/test_eiger_system.py
+++ tests/eiger/test_eiger_system.py
@@ -1,14 +1,16 @@
-import json
-
 import aiohttp
 import pytest
+
+DETECTOR_URL = "http://localhost:8081/detector/api/1.8.0/"
+FILE_WRITER_URL = "http://localhost:8081/filewriter/api/1.8.0/"
+MONITOR_URL = "http://localhost:8081/monitor/api/1.8.0/"
+STREAM_URL = "http://localhost:8081/stream/api/1.8.0/"
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "tickit_task", ["examples/configs/eiger/eiger.yaml"], indirect=True
 )
-# @pytest.mark.skip("do not merge while skipped!")
 async def test_eiger_system(tickit_task):
     commands = {
         "initialize": {"sequence id": 1},
@@ -17,16 +19,11 @@ async def test_eiger_system(tickit_task):
         "abort": {"sequence id": 6},
     }
 
-    url = "http://0.0.0.0:8081/detector/api/1.8.0/"
     headers = {"content-type": "application/json"}
 
-    filewriter_url = "http://0.0.0.0:8081/filewriter/api/1.8.0/"
-    monitor_url = "http://0.0.0.0:8081/monitor/api/1.8.0/"
-    stream_url = "http://0.0.0.0:8081/stream/api/1.8.0/"
-
     async def get_status(status, expected):
-        async with session.get(url + f"status/{status}") as resp:
-            assert expected == json.loads(str(await resp.text()))["value"]
+        async with session.get(DETECTOR_URL + f"status/{status}") as response:
+            assert expected == (await response.json())["value"]
 
     async with aiohttp.ClientSession() as session:
         await get_status(status="state", expected="na")
@@ -34,115 +31,124 @@ async def test_eiger_system(tickit_task):
         # Test setting config var before Eiger set up
         data = '{"value": "test"}'
         async with session.put(
-            url + "config/element", headers=headers, data=data
-        ) as resp:
-            assert json.loads(str(await resp.text())) == []
+            DETECTOR_URL + "config/element", headers=headers, data=data
+        ) as response:
+            assert (await response.json()) == []
 
         # Test each command
         for key, value in commands.items():
-            async with session.put(url + f"command/{key}") as resp:
-                assert value == json.loads(str(await resp.text()))
+            async with session.put(DETECTOR_URL + f"command/{key}") as response:
+                assert value == (await response.json())
 
+        # Check status
         await get_status(status="doesnt_exist", expected="None")
-
         await get_status(status="board_000/th0_temp", expected=24.5)
-
         await get_status(status="board_000/doesnt_exist", expected="None")
-
         await get_status(status="builder/dcu_buffer_free", expected=0.5)
-
         await get_status(status="builder/doesnt_exist", expected="None")
 
         # Test Eiger in IDLE state
         await get_status(status="state", expected="idle")
 
-        async with session.get(url + "config/doesnt_exist") as resp:
-            assert json.loads(str(await resp.text()))["value"] == "None"
+        # Test settings/getting config
+        async with session.get(DETECTOR_URL + "config/doesnt_exist") as response:
+            assert (await response.json())["value"] == "None"
 
-        data = '{"value": "test"}'
         async with session.put(
-            url + "config/doesnt_exist", headers=headers, data=data
-        ) as resp:
-            assert json.loads(str(await resp.text())) == []
+            DETECTOR_URL + "config/doesnt_exist",
+            headers=headers,
+            json={"value": "test"},
+        ) as response:
+            assert (await response.json()) == []
 
-        async with session.get(url + "config/element") as resp:
-            assert json.loads(str(await resp.text()))["value"] == "Co"
+        async with session.get(DETECTOR_URL + "config/element") as response:
+            assert (await response.json())["value"] == "Co"
 
-        data = '{"value": "Li"}'
         async with session.put(
-            url + "config/element", headers=headers, data=data
-        ) as resp:
-            assert json.loads(str(await resp.text())) == ["element"]
+            DETECTOR_URL + "config/element",
+            headers=headers,
+            json={"value": "Li"},
+        ) as response:
+            assert (await response.json()) == ["element"]
 
-        async with session.get(url + "config/photon_energy") as resp:
-            assert 54.3 == json.loads(str(await resp.text()))["value"]
+        async with session.get(DETECTOR_URL + "config/photon_energy") as response:
+            assert 54.3 == (await response.json())["value"]
 
-        async with session.get(filewriter_url + "config/mode") as resp:
-            assert "enabled" == json.loads(str(await resp.text()))["value"]
+        async with session.get(FILE_WRITER_URL + "config/mode") as response:
+            assert "enabled" == (await response.json())["value"]
 
-        data = '{"value": "enabled"}'
         async with session.put(
-            filewriter_url + "config/mode", headers=headers, data=data
-        ) as resp:
-            assert ["mode"] == json.loads(str(await resp.text()))
+            FILE_WRITER_URL + "config/mode",
+            headers=headers,
+            json={"value": "enabled"},
+        ) as response:
+            assert ["mode"] == (await response.json())
 
-        data = '{"value": "test"}'
         async with session.put(
-            filewriter_url + "config/test", headers=headers, data=data
-        ) as resp:
-            assert [] == json.loads(str(await resp.text()))
+            FILE_WRITER_URL + "config/test",
+            headers=headers,
+            json={"value": "test"},
+        ) as response:
+            assert [] == (await response.json())
 
-        async with session.get(filewriter_url + "status/state") as resp:
-            assert "ready" == json.loads(str(await resp.text()))["value"]
+        # Test filewriter, monitor and stream endpoints
+        async with session.get(FILE_WRITER_URL + "status/state") as response:
+            assert "ready" == (await response.json())["value"]
 
-        async with session.get(monitor_url + "config/mode") as resp:
-            assert "enabled" == json.loads(str(await resp.text()))["value"]
+        async with session.get(MONITOR_URL + "config/mode") as response:
+            assert "enabled" == (await response.json())["value"]
 
-        data = '{"value": "enabled"}'
         async with session.put(
-            monitor_url + "config/mode", headers=headers, data=data
-        ) as resp:
-            assert ["mode"] == json.loads(str(await resp.text()))
+            MONITOR_URL + "config/mode",
+            headers=headers,
+            json={"value": "enabled"},
+        ) as response:
+            assert ["mode"] == (await response.json())
 
-        data = '{"value": "test"}'
         async with session.put(
-            monitor_url + "config/test", headers=headers, data=data
-        ) as resp:
-            assert [] == json.loads(str(await resp.text()))
+            MONITOR_URL + "config/test",
+            headers=headers,
+            json={"value": "test"},
+        ) as response:
+            assert [] == (await response.json())
 
-        async with session.get(monitor_url + "status/error") as resp:
-            assert [] == json.loads(str(await resp.text()))["value"]
+        async with session.get(MONITOR_URL + "status/error") as response:
+            assert [] == (await response.json())["value"]
 
-        async with session.get(stream_url + "config/mode") as resp:
-            assert "enabled" == json.loads(str(await resp.text()))["value"]
+        async with session.get(STREAM_URL + "config/mode") as response:
+            assert "enabled" == (await response.json())["value"]
 
-        data = '{"value": "enabled"}'
         async with session.put(
-            stream_url + "config/mode", headers=headers, data=data
-        ) as resp:
-            assert ["mode"] == json.loads(str(await resp.text()))
+            STREAM_URL + "config/mode",
+            headers=headers,
+            json={"value": "enabled"},
+        ) as response:
+            assert ["mode"] == (await response.json())
 
-        data = '{"value": "test"}'
         async with session.put(
-            stream_url + "config/test", headers=headers, data=data
-        ) as resp:
-            assert [] == json.loads(str(await resp.text()))
+            STREAM_URL + "config/test",
+            headers=headers,
+            json={"value": "test"},
+        ) as response:
+            assert [] == (await response.json())
 
-        data = '{"value": "ints"}'
+        # Test acquisition in ints mode
         async with session.put(
-            url + "config/trigger_mode", headers=headers, data=data
-        ) as resp:
-            assert ["trigger_mode"] == json.loads(str(await resp.text()))
+            DETECTOR_URL + "config/trigger_mode",
+            headers=headers,
+            json={"value": "ints"},
+        ) as response:
+            assert ["trigger_mode"] == (await response.json())
 
-        async with session.get(stream_url + "status/state") as resp:
-            assert "ready" == json.loads(str(await resp.text()))["value"]
+        async with session.get(STREAM_URL + "status/state") as response:
+            assert "ready" == (await response.json())["value"]
 
         assert get_status(status="state", expected="idle")
 
-        async with session.put(url + "command/arm") as resp:
-            assert {"sequence id": 2} == json.loads(str(await resp.text()))
+        async with session.put(DETECTOR_URL + "command/arm") as response:
+            assert {"sequence id": 2} == (await response.json())
 
         assert get_status(status="state", expected="ready")
 
-        async with session.put(url + "command/trigger") as resp:
-            assert {"sequence id": 4} == json.loads(str(await resp.text()))
+        async with session.put(DETECTOR_URL + "command/trigger") as response:
+            assert {"sequence id": 4} == (await response.json())
