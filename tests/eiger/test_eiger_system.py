19a6bff06eb62ff178aca9c3e612769cae479b50
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-07-13 09:01:18 +0100

    Add timeouts to eiger system tests

--- tests/eiger/test_eiger_system.py
+++ tests/eiger/test_eiger_system.py
@@ -5,6 +5,7 @@ DETECTOR_URL = "http://localhost:8081/detector/api/1.8.0/"
 FILE_WRITER_URL = "http://localhost:8081/filewriter/api/1.8.0/"
 MONITOR_URL = "http://localhost:8081/monitor/api/1.8.0/"
 STREAM_URL = "http://localhost:8081/stream/api/1.8.0/"
+REQUEST_TIMEOUT = 1.0
 
 
 @pytest.mark.asyncio
@@ -22,22 +23,30 @@ async def test_eiger_system(tickit_task):
     headers = {"content-type": "application/json"}
 
     async def get_status(status, expected):
-        async with session.get(DETECTOR_URL + f"status/{status}") as response:
+        async with session.get(
+            DETECTOR_URL + f"status/{status}",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert expected == (await response.json())["value"]
 
     async with aiohttp.ClientSession() as session:
         await get_status(status="state", expected="na")
 
         # Test setting config var before Eiger set up
-        data = '{"value": "test"}'
         async with session.put(
-            DETECTOR_URL + "config/element", headers=headers, data=data
+            DETECTOR_URL + "config/element",
+            headers=headers,
+            json={"value": "test"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert (await response.json()) == []
 
         # Test each command
         for key, value in commands.items():
-            async with session.put(DETECTOR_URL + f"command/{key}") as response:
+            async with session.put(
+                DETECTOR_URL + f"command/{key}",
+                timeout=REQUEST_TIMEOUT,
+            ) as response:
                 assert value == (await response.json())
 
         # Check status
@@ -51,36 +60,51 @@ async def test_eiger_system(tickit_task):
         await get_status(status="state", expected="idle")
 
         # Test settings/getting config
-        async with session.get(DETECTOR_URL + "config/doesnt_exist") as response:
+        async with session.get(
+            DETECTOR_URL + "config/doesnt_exist",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert (await response.json())["value"] == "None"
 
         async with session.put(
             DETECTOR_URL + "config/doesnt_exist",
             headers=headers,
             json={"value": "test"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert (await response.json()) == []
 
-        async with session.get(DETECTOR_URL + "config/element") as response:
+        async with session.get(
+            DETECTOR_URL + "config/element",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert (await response.json())["value"] == "Co"
 
         async with session.put(
             DETECTOR_URL + "config/element",
             headers=headers,
             json={"value": "Li"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert (await response.json()) == ["element"]
 
-        async with session.get(DETECTOR_URL + "config/photon_energy") as response:
+        async with session.get(
+            DETECTOR_URL + "config/photon_energy",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert 54.3 == (await response.json())["value"]
 
-        async with session.get(FILE_WRITER_URL + "config/mode") as response:
+        async with session.get(
+            FILE_WRITER_URL + "config/mode",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert "enabled" == (await response.json())["value"]
 
         async with session.put(
             FILE_WRITER_URL + "config/mode",
             headers=headers,
             json={"value": "enabled"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert ["mode"] == (await response.json())
 
@@ -88,20 +112,28 @@ async def test_eiger_system(tickit_task):
             FILE_WRITER_URL + "config/test",
             headers=headers,
             json={"value": "test"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert [] == (await response.json())
 
         # Test filewriter, monitor and stream endpoints
-        async with session.get(FILE_WRITER_URL + "status/state") as response:
+        async with session.get(
+            FILE_WRITER_URL + "status/state",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert "ready" == (await response.json())["value"]
 
-        async with session.get(MONITOR_URL + "config/mode") as response:
+        async with session.get(
+            MONITOR_URL + "config/mode",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert "enabled" == (await response.json())["value"]
 
         async with session.put(
             MONITOR_URL + "config/mode",
             headers=headers,
             json={"value": "enabled"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert ["mode"] == (await response.json())
 
@@ -109,19 +141,27 @@ async def test_eiger_system(tickit_task):
             MONITOR_URL + "config/test",
             headers=headers,
             json={"value": "test"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert [] == (await response.json())
 
-        async with session.get(MONITOR_URL + "status/error") as response:
+        async with session.get(
+            MONITOR_URL + "status/error",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert [] == (await response.json())["value"]
 
-        async with session.get(STREAM_URL + "config/mode") as response:
+        async with session.get(
+            STREAM_URL + "config/mode",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert "enabled" == (await response.json())["value"]
 
         async with session.put(
             STREAM_URL + "config/mode",
             headers=headers,
             json={"value": "enabled"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert ["mode"] == (await response.json())
 
@@ -129,6 +169,7 @@ async def test_eiger_system(tickit_task):
             STREAM_URL + "config/test",
             headers=headers,
             json={"value": "test"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert [] == (await response.json())
 
@@ -137,18 +178,28 @@ async def test_eiger_system(tickit_task):
             DETECTOR_URL + "config/trigger_mode",
             headers=headers,
             json={"value": "ints"},
+            timeout=REQUEST_TIMEOUT,
         ) as response:
             assert ["trigger_mode"] == (await response.json())
 
-        async with session.get(STREAM_URL + "status/state") as response:
+        async with session.get(
+            STREAM_URL + "status/state",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert "ready" == (await response.json())["value"]
 
         assert get_status(status="state", expected="idle")
 
-        async with session.put(DETECTOR_URL + "command/arm") as response:
+        async with session.put(
+            DETECTOR_URL + "command/arm",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert {"sequence id": 2} == (await response.json())
 
         assert get_status(status="state", expected="ready")
 
-        async with session.put(DETECTOR_URL + "command/trigger") as response:
+        async with session.put(
+            DETECTOR_URL + "command/trigger",
+            timeout=REQUEST_TIMEOUT,
+        ) as response:
             assert {"sequence id": 4} == (await response.json())
