7fe63e378c7ae88328cc393ccc1540889838f7a9
callumforrester <callumforrester@users.noreply.github.com>
2023-07-14 14:28:32 +0000

    deploy: 65f7cec1fbc54cb582b9b13ef82214ddde72c3dc

--- eiger-exts-logic/_static/documentation_options.js
+++ eiger-exts-logic/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev52+ge24721c',
+    VERSION: '0.1.1.dev53+g65f7cec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
