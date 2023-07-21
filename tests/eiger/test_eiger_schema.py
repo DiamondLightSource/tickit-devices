34e601046c921f61be372636db8cb79d9c0f24f7
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-07-21 13:46:38 +0100

    Fix imports and mypy

--- tests/eiger/test_eiger_schema.py
+++ tests/eiger/test_eiger_schema.py
@@ -13,9 +13,13 @@ def test_sequence_complete_uses_alternative_constructor(sequence_id: int) -> Non
 @pytest.mark.parametrize("sequence_id", [1, 2])
 def test_sequence_complete_uses_space_in_field_name(sequence_id: int) -> None:
     complete = SequenceComplete.number(sequence_id)
-    assert serialize(complete)["sequence id"] == sequence_id
+    serialized = serialize(complete)
+    assert isinstance(serialized, dict)
+    assert serialized["sequence id"] == sequence_id
 
 
 def test_sequence_complete_uses_alias_only() -> None:
     complete = SequenceComplete.number(1)
-    assert "sequence_id" not in serialize(complete).keys()
+    serialized = serialize(complete)
+    assert isinstance(serialized, dict)
+    assert "sequence_id" not in serialized.keys()
