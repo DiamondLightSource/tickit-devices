7079a166454a32ea9332a4b6370d59d69ece251d
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-07-21 08:21:48 +0000

    deploy: 8dc0c30f0353b6f6cb8111deb46568ab4067896f

--- apischema/_static/documentation_options.js
+++ apischema/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev30+g067e9e0',
+    VERSION: '0.1.1.dev31+g8dc0c30',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
