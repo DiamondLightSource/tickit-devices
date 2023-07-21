c1bcaa764d9cbc8d79597787343e15f4aaeeaabe
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-07-21 10:47:39 +0000

    deploy: e21fdaa61a072b40345db34e55aa72997f386878

--- DiamondJoseph-patch-1/_static/documentation_options.js
+++ DiamondJoseph-patch-1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev59+g12f154e',
+    VERSION: '0.1.1.dev60+ge21fdaa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
