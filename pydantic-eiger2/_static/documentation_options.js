14be1db1706857baef2c56247764d8669d6f464b
callumforrester <callumforrester@users.noreply.github.com>
2023-07-24 15:32:55 +0000

    deploy: cdecffa827ea251187b7b2ca7d61669f742eb86f

--- pydantic-eiger2/_static/documentation_options.js
+++ pydantic-eiger2/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev48+g3b6bd9d',
+    VERSION: '0.1.1.dev49+gcdecffa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
