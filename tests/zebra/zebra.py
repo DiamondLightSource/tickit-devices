e7e9a6234decd4e31b5b2a8d715e784ce61bb668
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2023-08-16 10:03:33 +0100

    Fixed AndOrBlock tests

--- tests/zebra/zebra.py
+++ tests/zebra/zebra.py
@@ -1,7 +1,7 @@
 import pytest
 from tickit.core.typedefs import ComponentID, SimTime
 
-from tickit_devices.zebra.and_or_block import AndOrBlockConfig
+from tickit_devices.zebra.and_or_block import AndOrBlock
 
 all_true = 15
 one_false = 14
@@ -17,10 +17,10 @@ varying_values_of_true = {all_false, one_true, one_false, all_true}
 @pytest.mark.parametrize("inverted", varying_values_of_true)
 def test_and_block_enabled_or_inverted(enabled: int, inverted: int):
     name = ComponentID("AND1")
-    params = {f"{name}_ENA": enabled, f"{name}_INV": inverted}
     mux = {f"INP{i + 1}": True for i in range(4)}
-    block = AndOrBlockConfig(name, params)()
-    out = block.on_tick(SimTime(0), mux)
+    block = AndOrBlock(name=name)
+    block.params = {f"{name}_ENA": enabled, f"{name}_INV": inverted}
+    out = block.update(SimTime(0), mux)
     assert out.outputs == {
         "OUT": enabled + inverted == all_true
     }  # All devices either enabled or inverted
@@ -30,10 +30,10 @@ def test_and_block_enabled_or_inverted(enabled: int, inverted: int):
 @pytest.mark.parametrize("inverted", varying_values_of_true)
 def test_or_block_enabled_or_inverted(enabled: int, inverted: int):
     name = ComponentID("OR1")
-    params = {f"{name}_ENA": enabled, f"{name}_INV": inverted}
+    block = AndOrBlock(name=name)
+    block.params = {f"{name}_ENA": enabled, f"{name}_INV": inverted}
     mux = {f"INP{i + 1}": True for i in range(4)}
-    block = AndOrBlockConfig(name, params)()
-    out = block.on_tick(SimTime(0), mux)
+    out = block.update(SimTime(0), mux)
     assert out.outputs == {
         "OUT": enabled != inverted
     }  # At least one device exclusively enabled or inverted
@@ -42,18 +42,18 @@ def test_or_block_enabled_or_inverted(enabled: int, inverted: int):
 @pytest.mark.parametrize("high", varying_values_of_true)
 def test_all_enabled_and_block_for_inputs(high: int):
     name = ComponentID("AND1")
-    params = {f"{name}_ENA": 15, f"{name}_INV": 0}
+    block = AndOrBlock(name=name)
+    block.params = {f"{name}_ENA": all_true, f"{name}_INV": all_false}
     mux = {f"INP{i + 1}": bool(high & (1 << i)) for i in range(4)}
-    block = AndOrBlockConfig(name, params)()
-    out = block.on_tick(SimTime(0), mux)
+    out = block.update(SimTime(0), mux)
     assert out.outputs == {"OUT": high == all_true}  # All inputs high
 
 
 @pytest.mark.parametrize("high", varying_values_of_true)
 def test_all_enabled_or_block_for_inputs(high: int):
     name = ComponentID("OR1")
-    params = {f"{name}_ENA": 15, f"{name}_INV": 0}
+    block = AndOrBlock(name=name)
+    block.params = {f"{name}_ENA": all_true, f"{name}_INV": all_false}
     mux = {f"INP{i + 1}": bool(high & (1 << i)) for i in range(4)}
-    block = AndOrBlockConfig(name, params)()
-    out = block.on_tick(SimTime(0), mux)
+    out = block.update(SimTime(0), mux)
     assert out.outputs == {"OUT": high != all_false}  # At least one input high
