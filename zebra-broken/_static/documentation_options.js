3ca67a188bdfe568d410f6e96ad094cebbd94403
DiamondJoseph <DiamondJoseph@users.noreply.github.com>
2023-08-30 13:37:47 +0000

    deploy: bb836ccfbbea3e29cea62f23773837c81e417b03

--- zebra-broken/_static/documentation_options.js
+++ zebra-broken/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.1.dev6+ge7e9a62',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.3.1.dev8+gbb836cc',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
