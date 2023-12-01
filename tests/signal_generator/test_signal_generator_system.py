5b9f592eef0b2aef4fc0fef1085f0f8507d69e85
Callum Forrester <callum.forrester@diamond.ac.uk>
2023-12-01 13:46:49 +0000

    Add test of signal

--- tests/signal_generator/test_signal_generator_system.py
+++ tests/signal_generator/test_signal_generator_system.py
@@ -1,8 +1,9 @@
 import asyncio
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, List
 
 import aioca
+import numpy as np
 import pytest
 
 PV_PREFIX = "SIGNALGEN"
@@ -16,6 +17,8 @@ GATE_THRESHOLD = f"{PV_PREFIX}:GateThreshold"
 GATE_THRESHOLD_RBV = f"{GATE_THRESHOLD}_RBV"
 ENABLED = f"{PV_PREFIX}:Enabled"
 ENABLED_RBV = f"{ENABLED}_RBV"
+SIGNAL_RBV = f"{PV_PREFIX}:Signal_RBV"
+GATE_RBV = f"{PV_PREFIX}:Gate_RBV"
 
 
 @pytest.mark.asyncio
@@ -30,6 +33,7 @@ async def test_signal_generator_system(tickit_process):
     await set_and_test(FREQUENCY, FREQUENCY, 1.0, 2.0)
     await set_and_test(GATE_THRESHOLD, GATE_THRESHOLD_RBV, 0.5, 0.25)
     await set_and_test(ENABLED, ENABLED_RBV, True, False)
+    await check_signal_and_gate(0.25)
 
 
 async def set_and_test(pv: str, rbv: str, expected_initial_value: Any, new_value: Any):
@@ -43,9 +47,19 @@ async def set_and_test(pv: str, rbv: str, expected_initial_value: Any, new_value
     assert initial_value == expected_initial_value, (
         f"Initially caget({pv}) was {initial_value}, expected {expected_initial_value}",
     )
-    assert actual_demand_value == new_value, (
-        f"After caput({pv}, {new_value}), {pv} was actually {actual_new_value}"
-    )
-    assert actual_new_value == new_value, (
-        f"After caput({pv}, {new_value}), {rbv} was actually {actual_new_value}"
-    )
+    assert (
+        actual_demand_value == new_value
+    ), f"After caput({pv}, {new_value}), {pv} was actually {actual_new_value}"
+    assert (
+        actual_new_value == new_value
+    ), f"After caput({pv}, {new_value}), {rbv} was actually {actual_new_value}"
+
+
+async def check_signal_and_gate(threshold: float):
+    for _ in range(30):
+        signal, gate = await asyncio.gather(
+            *[aioca.caget(SIGNAL_RBV), aioca.caget(GATE_RBV)]
+        )
+        should_gate_be_open = signal >= threshold
+        assert should_gate_be_open == gate
+        await asyncio.sleep(0.1)
